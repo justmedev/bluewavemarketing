3c5fca9caac1ed7f5b568c8bbfe9748ca6af24a5
justmedev <ilja.ragnar@gmail.com>
2023-09-06 08:55:30 +0200

    add footer

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,40 @@ import Home from "./views/Home.vue";
 
 <template>
   <Navbar/>
-  <div class="container">
+  <main class="container">
     <div>
       <Home/>
     </div>
-  </div>
+  </main>
+  <footer>
+    <div>
+      <div>
+        &copy; bluewavemarketing.at 2023 - present &ctdot; All rights reserved
+      </div>
+      <div>
+
+      </div>
+    </div>
+  </footer>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+footer {
+  user-select: none;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background: #1a1a1a;
+  color: #c2c2c2;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  > div {
+    margin: 5px 10px;
+  }
+}
+
 .container {
   width: 100vw;
   height: 100%;
